9df11909997c3297e83eef1c7859c9465158c14f
Junyuan Chen <75352049+JunyuanChen@users.noreply.github.com>
2021-10-20 09:21:34 -0400

    Explain EXP vs total EXP in calc_exp.py docstring

--- concerns/calc_exp.py
+++ concerns/calc_exp.py
@@ -2,6 +2,24 @@
 
 """
 Calculations for EXP and levels.
+
+EXP vs total EXP
+================
+EXP refers to EXP at current level.  When EXP reaches a certain value
+determined by exp_requirement(), the user will level up, and EXP will be
+reset to 0.  However, total EXP takes level into consideration.  It will
+NOT reset when user levels up.
+
+Example:
+exp_requirement(0) = 1000, this means users at level 0 need 1000 EXP to
+level up.  Suppose current level = 0, EXP = 900.  The total EXP is 900.
+If the user gained another 200 EXP, EXP = 1100 > 1000, so the user will
+level up.  Now level = 1 and EXP = 1100 - 1000 = 100, but total EXP is
+not reset, thus total EXP = 1100.
+
+To calculate total EXP from EXP, just add exp_requirement for all level
+below user's level.  This is the total number of EXP deducted from the
+user for levelling up.  Adding them back to EXP gives total EXP.
 """
 
 
