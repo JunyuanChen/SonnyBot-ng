b91c5ee84b796ecffe45e03bedee92354252f574
1maeth2 <1maeth2@hdsb.ca>
2021-11-01 23:04:46 -0400

    Fixed message deletion

--- main.py
+++ main.py
@@ -461,10 +461,11 @@ async def on_message(message: discord.Message):
     
     ctx = await bot.get_context(message)
 
-    await bot.process_commands(message)
-
     if ctx.command:
         await ctx.message.delete()
+
+    await bot.process_commands(message)
+
 
 
 @bot.event
